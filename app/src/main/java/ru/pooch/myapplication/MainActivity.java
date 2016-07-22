7f59d0848a6549b315b8168042f6dc3a4cf3f93e
Pooch <libiren@yandex.ua>
2016-07-22 20:51:53 +0300

    [NT-1232] Refactoring of the code

--- app/src/main/java/ru/pooch/myapplication/MainActivity.java
+++ app/src/main/java/ru/pooch/myapplication/MainActivity.java
@@ -1,6 +1,4 @@
 package ru.pooch.myapplication;
-
-
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -11,17 +9,12 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.MalformedJsonException;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import retrofit.Call;
 import retrofit.GsonConverterFactory;
 import retrofit.Response;
@@ -30,18 +23,15 @@ import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
     private EditText text;
-    private TextView translated;
-    private TextView textRus;
-    private TextView textEng;
-
+    private TextView mTranslatedText;
+    private TextView rusText;
+    private TextView engText;
     private boolean language = true;
-    Map<String, String> mapJson = new HashMap<String, String>();
-
-
+    Map<String, String> mapJson = new HashMap<>();
     private ImageButton changeBtn;
     private final String KEY = "trnsl.1.1.20160530T163721Z.46212086c960f3a8.b52ad3378730521c12680e290eba0d085ba50181";
     private final String URL = "https://translate.yandex.net";
-    private final String FORMAT = "plain";
+
     private Gson gson = new GsonBuilder().create();
     private Retrofit retrofit = new Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create(gson))
@@ -49,24 +39,21 @@ public class MainActivity extends AppCompatActivity {
             .build();
     private Link intf = retrofit.create(Link.class);
 
-//    /**
-//     * ATTENTION: This was auto-generated to implement the App Indexing API.
-//     * See https://g.co/AppIndexing/AndroidStudio for more information.
-//     */
-//    private GoogleApiClient client;
-
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (EditText) findViewById(R.id.editText);
+        rusText = (TextView) findViewById(R.id.textRu);
+        engText = (TextView) findViewById(R.id.textEn);
+        mTranslatedText = (TextView) findViewById(R.id.textView);
+        changeBtn = (ImageButton) findViewById(R.id.buttonChange);
         mapJson.put("lang", "ru-en");
 
         text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (text != null) {
-                    text.setText("");
+                    text.getText().clear();
                 }
             }
         });
@@ -79,21 +66,17 @@ public class MainActivity extends AppCompatActivity {
                     String request = text.getText().toString();
                     request = request.replace(" ", "+");
                     mapJson.put("text", request);
-
                     Call<Object> call = intf.translate(mapJson);
                     StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
                     StrictMode.setThreadPolicy(policy);
                     try {
                         Response<Object> response = call.execute();
-
                         Map<String, String> map = gson.fromJson(response.body().toString(), Map.class);
                         for (Map.Entry e : map.entrySet()) {
                             if (e.getKey().equals("text")) {
-                                translated.setText(e.getValue().toString().replace("+", " ").replace("[", "").replace("]", "").replace("\n", ""));
+                                mTranslatedText.setText(e.getValue().toString().replace("+", " ").replace("[", "").replace("]", "").replace("\n", ""));
                                 InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
                                 imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-
-
                             }
                         }
                     } catch (MalformedJsonException e) {
@@ -102,34 +85,26 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-
-
                 return false;
             }
         });
 
-
-        textRus = (TextView) findViewById(R.id.textRu);
-        textEng = (TextView) findViewById(R.id.textEn);
-        translated = (TextView) findViewById(R.id.textView);
-
-        changeBtn = (ImageButton) findViewById(R.id.buttonChange);
         changeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!language) {
-                    textRus.setText("EN");
-                    textEng.setText("RU");
+                    rusText.setText(R.string.en);
+                    engText.setText(R.string.ru);
                     mapJson.put("lang", "en-ru");
                     language = true;
                 } else {
-                    textRus.setText("RU");
-                    textEng.setText("EN");
+                    rusText.setText(R.string.ru);
+                    engText.setText(R.string.en);
                     mapJson.put("lang", "ru-en");
                     language = false;
                 }
             }
         });
-//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 }
